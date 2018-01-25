5e7ef032c16355fa0743de55a8ba908f33e919bd
Emmanuel Olivo <emmanuel@mmacbook-con-dos-emes.local>
2018-01-24 20:02:35 -0600

    Import CoreLocation library Add APP_ID and locationManager delegate Add Location usage request

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -7,16 +7,18 @@
 //
 
 import UIKit
+import CoreLocation
 
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
-    let APP_ID = "e72ca729af228beabd5d20e3b7749713"
+    let APP_ID = "785096aa059c8dd11659d9b7de13f1c6"
     
 
     //TODO: Declare instance variables here
+    let locationManager = CLLocationManager()
     
 
     
@@ -31,6 +33,9 @@ class WeatherViewController: UIViewController {
         
         
         //TODO:Set up the location manager here.
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
     
         
         
